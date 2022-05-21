67ed50d2768764366d868625d9f07c114e1062eb
Cr4yfish <58395553+cr4yfish@users.noreply.github.com>
2022-05-22 00:26:12 +0200

    added local scenes refresh on saving new

--- public/scripts/scenes.js
+++ public/scripts/scenes.js
@@ -66,7 +66,12 @@ function saveCurrentScene(buttonE) {
         console.log(response);
         closeOverlay();
         dashboardScene();
+        refreshLocalScenes();
     })
+}
+
+const refreshLocalScenes = () => {
+    localStorage.clear("scenes");
 }
 
 function getScenes() {
